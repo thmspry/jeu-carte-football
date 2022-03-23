7bb5d4fafd7e4a5ac46abb1ae893ff52e1406c81
Thomas Peray <thomas.peray5@gmail.com>
2022-03-23 11:22:06 +0100

    Fenetre Admin, logo de fenetre

--- src/main/java/com/javafootball/Application.java
+++ src/main/java/com/javafootball/Application.java
@@ -2,9 +2,11 @@ package com.javafootball;
 
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
 import java.io.IOException;
+import java.util.Objects;
 
 public class Application extends javafx.application.Application {
     @Override
@@ -12,6 +14,7 @@ public class Application extends javafx.application.Application {
         FXMLLoader fxmlLoader = new FXMLLoader(Application.class.getResource("Connexion.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 1080, 720);
         stage.setTitle("Zimdim Football");
+        stage.getIcons().add(new Image(Objects.requireNonNull(Application.class.getResourceAsStream("zimdim_coin.png"))));
         stage.setScene(scene);
         stage.show();
     }
