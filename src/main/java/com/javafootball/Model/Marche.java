84cd7bf55ac951420e1219c54a7e638e9b8ca791
Thomas Peray <thomas.peray5@gmail.com>
2022-03-30 18:56:29 +0200

    Classe Marché

---
+++ src/main/java/com/javafootball/Model/Marche.java
@@ -0,0 +1,14 @@
+package com.javafootball.Model;
+
+import com.javafootball.Model.Joueur.Carte;
+
+import java.util.List;
+
+public class Marche {
+    List<Carte> carteAVendre;
+
+    public Marche(List<Carte> carteAVendre) {
+        this.carteAVendre = carteAVendre;
+    }
+}
+
